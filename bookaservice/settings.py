6eb718509df73abb88221e2dfeb0b8ae369bffba
GDV373 <georgeattard144@gmail.com>
2023-09-27 20:05:55 +0000

    new static fix

--- bookaservice/settings.py
+++ bookaservice/settings.py
@@ -177,7 +177,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # Static files (CSS, JavaScript, Images)
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')	
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_files')	
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
