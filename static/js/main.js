90fd2e6387816bee7fb426db77936bb769841d08
GDV373 <georgeattard144@gmail.com>
2023-10-01 08:27:42 +0000

    added required to needed fields and JS to handel

--- static/js/main.js
+++ static/js/main.js
@@ -10,11 +10,9 @@
         }, 1);
     };
     spinner();
-    
-    
+
     // Initiate the wowjs
     new WOW().init();
-
 
     // Sticky Navbar
     $(window).scroll(function () {
@@ -35,38 +33,36 @@
 
         if (windowsPathname === navLinkPathname) {
             navLinkEl.classList.add('active');
-        }    
+        }
     });
-    
-    
+
     // Dropdown on mouse hover
     const $dropdown = $(".dropdown");
     const $dropdownToggle = $(".dropdown-toggle");
     const $dropdownMenu = $(".dropdown-menu");
     const showClass = "show";
-    
-    $(window).on("load resize", function() {
+
+    $(window).on("load resize", function () {
         if (this.matchMedia("(min-width: 992px)").matches) {
             $dropdown.hover(
-            function() {
-                const $this = $(this);
-                $this.addClass(showClass);
-                $this.find($dropdownToggle).attr("aria-expanded", "true");
-                $this.find($dropdownMenu).addClass(showClass);
-            },
-            function() {
-                const $this = $(this);
-                $this.removeClass(showClass);
-                $this.find($dropdownToggle).attr("aria-expanded", "false");
-                $this.find($dropdownMenu).removeClass(showClass);
-            }
+                function () {
+                    const $this = $(this);
+                    $this.addClass(showClass);
+                    $this.find($dropdownToggle).attr("aria-expanded", "true");
+                    $this.find($dropdownMenu).addClass(showClass);
+                },
+                function () {
+                    const $this = $(this);
+                    $this.removeClass(showClass);
+                    $this.find($dropdownToggle).attr("aria-expanded", "false");
+                    $this.find($dropdownMenu).removeClass(showClass);
+                }
             );
         } else {
             $dropdown.off("mouseenter mouseleave");
         }
     });
-    
-    
+
     // Back to top button
     $(window).scroll(function () {
         if ($(this).scrollTop() > 300) {
@@ -76,17 +72,15 @@
         }
     });
     $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        $('html, body').animate({ scrollTop: 0 }, 1500, 'easeInOutExpo');
         return false;
     });
-
 
     // Facts counter
     $('[data-toggle="counter-up"]').counterUp({
         delay: 10,
         time: 2000
     });
-
 
     // Date and time picker
     $('.date').datetimepicker({
@@ -96,7 +90,6 @@
         format: 'LT'
     });
 
-
     // Testimonials carousel
     $(".testimonial-carousel").owlCarousel({
         autoplay: true,
@@ -105,19 +98,39 @@
         margin: 25,
         dots: true,
         loop: true,
-        nav : false,
+        nav: false,
         responsive: {
-            0:{
-                items:1
+            0: {
+                items: 1
             },
-            768:{
-                items:2
+            768: {
+                items: 2
             },
-            992:{
-                items:3
+            992: {
+                items: 3
             }
         }
     });
-    
-})(jQuery);
 
+    // Form validation
+    document.getElementById('booking-form').addEventListener('submit', function (event) {
+        // Get the form fields
+        var providerSelect = document.getElementById('provider');
+        var serviceSelect = document.getElementById('service');
+        var carSelect = document.getElementById('car');
+        var dateInput = document.querySelector('input[name="date"]');
+
+        // Check if any of the required fields are empty
+        if (!providerSelect.value || providerSelect.value === 'Select A Service Provider' ||
+            !serviceSelect.value || serviceSelect.value === 'Select A Service' ||
+            !carSelect.value || carSelect.value === 'Select Your Car' ||
+            !dateInput.value) {
+            // Prevent form submission
+            event.preventDefault();
+
+            // Show an error message
+            alert('Please fill out all required fields before booking.');
+        }
+    });
+
+})(jQuery);
