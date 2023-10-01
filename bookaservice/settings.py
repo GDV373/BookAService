13f5fe9c7b29c880b1879d9cf47ce156d0fd5017
GDV373 <georgeattard144@gmail.com>
2023-10-01 14:52:48 +0000

    change of secret key

--- bookaservice/settings.py
+++ bookaservice/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-t8fo$ytz=ms%v!lrhm*@3u)wui6nz8$)$_mq-%10lrzothj6(n'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
