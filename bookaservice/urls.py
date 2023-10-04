200ccce0d6886af99704ed95f97ef10c7ce0cdcd
GDV373 <georgeattard144@gmail.com>
2023-10-04 10:12:29 +0000

    fix bugs for 404 pages

--- bookaservice/urls.py
+++ bookaservice/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 
+handler505 = 'authentication.views.custom_505_handler'
+handler404 = 'authentication.views.custom_error_handler'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('authentication.urls')),
