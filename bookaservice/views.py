63db23b12d996629a4e3cfedc43710422026ba99
GDV373 <georgeattard144@gmail.com>
2023-10-07 14:31:44 +0000

    readded view url for app

--- bookaservice/views.py
+++ bookaservice/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def custom_500_handler(request, exception=None):
+    return render(request, 'page_404.html', status=500)
+
+
+def custom_error_handler(request, exception=None):
+    if exception is None:
+        return render(request, 'page_404.html', status=404)
+    else:
+        return render(request, 'page_404.html', status=404)
