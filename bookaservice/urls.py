b704f3b5c2bd4863269591a75879c2d8fa38ace1
GDV373 <georgeattard144@gmail.com>
2023-10-07 14:29:52 +0000

    remove view url for app

--- bookaservice/urls.py
+++ bookaservice/urls.py
@@ -1,17 +1,2 @@
 from django.contrib import admin
 from django.urls import path, include
-from authentication.views import (
-    custom_500_handler,
-    custom_error_handler
-)
-
-handler500 = 'authentication.views.custom_500_handler'
-handler404 = 'authentication.views.custom_error_handler'
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('authentication.urls')),
-    path('', include('base.urls')),
-    path('', include('client.urls')),
-    path('', include('business.urls')),
-]
